183dcd726631873174325407ff0ac44c5e04707c
Paige Anderson <paige.m.anderson1999@gmail.com>
2023-06-07 03:43:00 -0700

    assignment-ready commit

--- paigeanderson-assignment-9/src/main/java/com/coderscampus/assignment9/service/FileService.java
+++ paigeanderson-assignment-9/src/main/java/com/coderscampus/assignment9/service/FileService.java
@@ -1,5 +1,56 @@
 package com.coderscampus.assignment9.service;
 
-public class FileService {
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Service;
+
+import com.coderscampus.assignment9.domain.Recipes;
+
+@Service
+public class FileService {
+	
+	
+	public List<Recipes> readRecipes() throws IOException{
+		List <Recipes> recipeList = new ArrayList<>();
+		
+		Reader in = new FileReader("recipes.txt");
+		@SuppressWarnings("deprecation")
+		Iterable<CSVRecord> records = CSVFormat.DEFAULT.withDelimiter(',')
+													   .withEscape('\\')
+													   .withFirstRecordAsHeader()
+													   .withSkipHeaderRecord()
+													   .withIgnoreSurroundingSpaces()
+													   .parse(in);
+											   
+			for (CSVRecord record : records) {	       
+		        Integer cookingMinutes = Integer.parseInt(record.get(0));
+		        Boolean dairyFree = Boolean.parseBoolean(record.get(1));
+		        Boolean glutenFree = Boolean.parseBoolean(record.get(2));
+		        String instructions  = record.get(3);
+		        Double preparationMinutes = Double.parseDouble(record.get(4));
+		        Double pricePerServing = Double.parseDouble(record.get(5));
+		        Integer readyInMinutes = Integer.parseInt(record.get(6));
+		        Integer servings = Integer.parseInt(record.get(7));
+		        Double spoonacularScore = Double.parseDouble(record.get(8));
+		        String title = record.get(9);
+		        Boolean vegan = Boolean.parseBoolean(record.get(10));
+		        Boolean vegatarian = Boolean.parseBoolean(record.get(11));
+		        Recipes recipe = new Recipes
+		        		(cookingMinutes, dairyFree, glutenFree,
+		        		 instructions, preparationMinutes,
+		        		 pricePerServing, readyInMinutes,
+		        		 servings, spoonacularScore, title,
+		        		 vegan, vegatarian);
+		        recipeList.add(recipe);
+		    }
+		
+		return recipeList;
+	}
+	
 }
--- paigeanderson-assignment-9/src/main/java/com/coderscampus/assignment9/web/RecipeController.java
+++ paigeanderson-assignment-9/src/main/java/com/coderscampus/assignment9/web/RecipeController.java
@@ -1,5 +1,58 @@
 package com.coderscampus.assignment9.web;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coderscampus.assignment9.domain.Recipes;
+import com.coderscampus.assignment9.service.FileService;
+
+@RestController
 public class RecipeController {
+	
+	@Autowired
+	private FileService fileService;
+	
+	@GetMapping("/all-recipes")
+	public List<Recipes> readRecipes () throws IOException {
+		return fileService.readRecipes();
+	}
+ 	
+	@GetMapping("/gluten-free")
+	public List<Recipes> glutenFreeRecipes() throws IOException {
+		return fileService.readRecipes()
+						  .stream()
+						  .filter(Recipes::getGlutenFree)
+						  .collect(Collectors.toList());
+	}
+	
+	@GetMapping("/vegan")
+	public List<Recipes> veganRecipes() throws IOException {
+		return fileService.readRecipes()
+						  .stream()
+						  .filter(Recipes::getVegan)
+						  .collect(Collectors.toList());
+	}
+
+	@GetMapping("/vegan-and-gluten-free")
+	public List<Recipes> veganAndGlutenFreeRecipes() throws IOException {
+		return fileService.readRecipes()
+						  .stream()
+						  .filter(Recipes::getVegan)
+						  .filter(Recipes::getGlutenFree)
+						  .collect(Collectors.toList());
+	}
+
+	@GetMapping("/vegetarian")
+	public List<Recipes> vegetarianRecipes() throws IOException {
+		return fileService.readRecipes()
+						  .stream()
+						  .filter(Recipes::getVegetarian)
+						  .collect(Collectors.toList());
+	}
 
 }
